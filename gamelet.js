9eb618e54e151c9bc46ebdfedec9eabcf29c3eb8
Fola Adewole <folijok@gmail.com>
2023-07-19 16:28:32 +0100

    first commit

---
+++ gamelet.js
@@ -0,0 +1,25 @@
+const ball = document.getElementById('ball');
+document.addEventListener('keydown', handleKeyPress);
+let position = 0;
+
+function handleKeyPress(e) {
+    if (e.code === 'ArrowDown'){
+        position = position + 10;
+    }
+    if (e.code === 'ArrowUp') {
+        position = position - 10; 
+    }
+    if (e.code === 'ArrowDown')  {
+        position = position 
+    }
+    if (position < 0) {
+        position = 0;
+    }
+    refresh();
+    }
+    function refresh() {
+        ball.style.top = position + 'px';
+        ball.style.left = position + 'px';
+    }
+
+    
